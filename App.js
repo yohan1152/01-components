7c0f2af818ef073f3777839a4a7f4370c0be4aae
Yohan Puerta <yohanjjfray@gmail.com>
2020-02-24 19:48:19 -0500

    Se agregan funciones con flexbox y se crea una división de 4 Views

--- App.js
+++ App.js
@@ -4,17 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.box1}>
-        <Text style={styles.text}>Vista 1</Text>
-      </View>
-      <View style={styles.box2}>
-        <Text style={styles.text}>Vista 2</Text>
-      </View>
-      <View style={styles.box3}>
-        <Text style={styles.text}>Vista 3</Text>
-      </View>
 
-
+      <View style={styles.container1}>
+        <View style={styles.box1}>
+          <Text style={styles.text}>Vista 1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text style={styles.text}>Vista 2</Text>
+        </View>
+      </View>
+      <View style={styles.container2}>
+        <View style={styles.box3}>
+          <Text style={styles.text}>Vista 3</Text>
+        </View>
+        <View style={styles.box4}>
+          <Text style={styles.text}>Vista 4</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -25,19 +31,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flexDirection: 'column'
   },
+  container1: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  container2: {
+    flex: 1,
+    flexDirection: 'row'
+  },
   text: {
     color: '#ff1744'
   },
   box1: {
-    flex: 2,
+    flex: 1,
     backgroundColor: '#ffc107'
   },
   box2: {
-    flex: 2,
+    flex: 1,
     backgroundColor: '#0d47a1'
   },
   box3: {
     flex: 1,
     backgroundColor: '#d50000'
+  },
+  box4: {
+    flex: 1,
+    backgroundColor: '#49D79D'
   }
 });
